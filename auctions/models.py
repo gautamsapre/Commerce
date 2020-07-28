00134d012c31fe2fef00b175828f4d3498d06f3a
Gautam Sapre <gautamsapre@gmail.com>
2020-07-27 19:51:51 -0500

    Foreign key listings working

--- auctions/models.py
+++ auctions/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class listings(models.Model):
+    name = models.CharField(max_length = 64)
+    price = models.IntegerField()
+
 class User(AbstractUser):
+    listing = models.ForeignKey(listings, on_delete=models.CASCADE, related_name="active_listings", default = "", null=True)
     def __str__(self):
-        return f"{self.username}: {self.email}"
+        return f"{self.username}: {self.email} {self.id}"
