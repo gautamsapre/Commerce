30e743c63f6976d4e70a07ee8bf74fb63a899f2d
Gautam Sapre <gautamsapre@gmail.com>
2020-07-28 11:57:46 -0500

    reversed listing foreignkey, added listings on html page

--- auctions/models.py
+++ auctions/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username}: {self.email} {self.id}"
+
 
 class listings(models.Model):
     name = models.CharField(max_length = 64)
     price = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="active_listings", default = "", null=True)
 
-class User(AbstractUser):
-    listing = models.ForeignKey(listings, on_delete=models.CASCADE, related_name="active_listings", default = "", null=True)
     def __str__(self):
-        return f"{self.username}: {self.email} {self.id}"
+        return f"{self.name}: \nPrice: {self.price} \nBy: {self.user.username}"
